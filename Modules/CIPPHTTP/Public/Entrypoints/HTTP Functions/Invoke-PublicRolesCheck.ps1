c80a74932a4804d00bbb352a3ff54b4866a68fbb
jaiwcit <jai@westcoastit.com.au>
2026-05-05 10:23:33 +0800

    Invoke-PublicRolesCheck in honored API path

---
+++ Modules/CIPPHTTP/Public/Entrypoints/HTTP Functions/Invoke-PublicRolesCheck.ps1
@@ -0,0 +1,45 @@
+using namespace System.Net
+using namespace Microsoft.Azure.Functions.PowerShellWorker
+
+function Invoke-PublicRolesCheck {
+    [CmdletBinding()]
+    param(
+        $Request,
+        $TriggerMetadata
+    )
+
+    try {
+        $allowedRoles = @(
+            'superadmin',
+            'admin',
+            'editor',
+            'readonly'
+        )
+
+        $roleClaimTypes = @(
+            'roles',
+            'role',
+            'http://schemas.microsoft.com/ws/2008/06/identity/claims/role'
+        )
+
+        $claims = @($Request.Body.claims)
+
+        $rolesToReturn = $claims |
+            Where-Object { $_.typ -in $roleClaimTypes -and $_.val } |
+            ForEach-Object { $_.val } |
+            Where-Object { $_ -in $allowedRoles } |
+            Sort-Object -Unique
+
+        return ([HttpResponseContext]@{
+            StatusCode = [HttpStatusCode]::OK
+            Headers    = @{ 'Content-Type' = 'application/json' }
+            Body       = @{ roles = @($rolesToReturn) } | ConvertTo-Json -Depth 5 -Compress
+        })
+    } catch {
+        return ([HttpResponseContext]@{
+            StatusCode = [HttpStatusCode]::OK
+            Headers    = @{ 'Content-Type' = 'application/json' }
+            Body       = @{ roles = @() } | ConvertTo-Json -Depth 5 -Compress
+        })
+    }
+}
