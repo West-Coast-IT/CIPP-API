05ce2127dd033c89a5b30edab0525c33898c64bf
jaiwcit <jai@westcoastit.com.au>
2026-05-05 11:57:47 +0800

    Update rolescheck to follow CIPP function formatting

--- Modules/CIPPHTTP/Public/Entrypoints/HTTP Functions/Invoke-PublicRolesCheck.ps1
+++ Modules/CIPPHTTP/Public/Entrypoints/HTTP Functions/Invoke-PublicRolesCheck.ps1
@@ -1,54 +1,46 @@
-using namespace System.Net
-using namespace Microsoft.Azure.Functions.PowerShellWorker
-
 function Invoke-PublicRolesCheck {
+    <#
+    .FUNCTIONALITY
+        Entrypoint
+    .ROLE
+        Public
+    #>
     [CmdletBinding()]
     param(
         $Request,
-        $TriggerMetadata,
-
-        [string[]]$Roles = @(
-            'anonymous',
-            'authenticated',
-            'superadmin',
-            'admin',
-            'editor',
-            'readonly'
-        )
+        $TriggerMetadata
     )
 
-    try {
-        $allowedRoles = @(
-            'superadmin',
-            'admin',
-            'editor',
-            'readonly'
-        )
+    $allowedRoles = @(
+        'superadmin',
+        'admin',
+        'authenticated',
+        'anonymous',
+        'editor',
+        'readonly'
+    )
 
-        $roleClaimTypes = @(
-            'roles',
-            'role',
-            'http://schemas.microsoft.com/ws/2008/06/identity/claims/role'
-        )
+    $roleClaimTypes = @(
+        'roles',
+        'role',
+        'http://schemas.microsoft.com/ws/2008/06/identity/claims/role'
+    )
 
-        $claims = @($Request.Body.claims)
+    $claims = @($Request.Body.claims)
 
-        $rolesToReturn = $claims |
-            Where-Object { $_.typ -in $roleClaimTypes -and $_.val } |
-            ForEach-Object { $_.val } |
-            Where-Object { $_ -in $allowedRoles } |
-            Sort-Object -Unique
+    $rolesToReturn = $claims |
+        Where-Object { $_.typ -in $roleClaimTypes -and $_.val } |
+        ForEach-Object { $_.val } |
+        Where-Object { $_ -in $allowedRoles } |
+        Sort-Object -Unique
 
-        return ([HttpResponseContext]@{
-            StatusCode = [HttpStatusCode]::OK
-            Headers    = @{ 'Content-Type' = 'application/json' }
-            Body       = @{ roles = @($rolesToReturn) } | ConvertTo-Json -Depth 5 -Compress
-        })
-    } catch {
-        return ([HttpResponseContext]@{
-            StatusCode = [HttpStatusCode]::OK
-            Headers    = @{ 'Content-Type' = 'application/json' }
-            Body       = @{ roles = @() } | ConvertTo-Json -Depth 5 -Compress
-        })
+    $Body = @{
+        roles = @($rolesToReturn)
+    }
+
+    return [HttpResponseContext]@{
+        StatusCode = [HttpStatusCode]::OK
+        Headers    = @{ 'Content-Type' = 'application/json' }
+        Body       = ($Body | ConvertTo-Json -Depth 5)
     }
 }
